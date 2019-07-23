3d7399a5341edde226f0ff8fc6c092677c7dbfed
samaswin <50949606+samaswin87@users.noreply.github.com>
2019-07-23 09:26:50 +0530

    added iframe example

---
+++ iframe-example.js
@@ -0,0 +1,12 @@
+$("#button").click(function () { 
+    $("#frame").attr("src", "https://samaswin87.github.io/");
+});
+
+
+<!-- IN HTML PAGE
+  <div id="mydiv">
+       <iframe id="frame" src="" width="100%" height="300">
+       </iframe>
+   </div>
+   <button id="button">Load</button>
+-->
